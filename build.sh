4aa3ee2850290e0f9288baf96b67fe62b3f702dd
Niall Gallagher <gallagher_niall@yahoo.com>
2021-10-14 20:23:09 +0100

    Update the build

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Jenkins is messed up
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+nvm list
+nvm use "v15.7.0"
+
+yarn
+yarn build
+yarn build:docker
